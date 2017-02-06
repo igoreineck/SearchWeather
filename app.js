4a9863069302c65780b4ea2c03897d7ac9af0a09
Igor Eineck <hauzip@gmail.com>
2017-02-06 00:23:37 -0200

    Upload 0.3

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var request = require('request'),
     express = require('express'),
     app     = express();
 
+
 app.set('port', (process.env.PORT || 3000));
 app.set('view engine', 'ejs');
 
@@ -21,6 +22,20 @@ app.get("/index", function(req, res){
         if (!error && response.statusCode == 200) {
             var data = JSON.parse(body);
             res.render("index", {data: data});
+        }
+    });
+});
+
+app.get("/index/page2", function(req, res){
+    var city_name = req.query.city,
+        country_name = req.query.country;
+        celsius = '&units=metric',
+        appi_key = ["\x26\x61\x70\x70\x69\x64\x3D\x66\x66\x33\x64\x32\x39\x65\x32\x33\x61\x39\x31\x63\x64\x31\x62\x39\x38\x30\x37\x31\x33\x33\x30\x32\x38\x32\x66\x35\x36\x36\x66"];
+        url = 'http://api.openweathermap.org/data/2.5/forecast?q=' + city_name + ',' + country_name + celsius + appi_key;
+    request(url, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var forecast = JSON.parse(body);
+            res.render("page2", {data: forecast});
         }
     });
 });
